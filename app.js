7d037eb88c3c3417169cb5573e400f1dc7291ba8
MouaouiaB <m-maroc-1998@hotmail.com>
2019-05-12 20:13:42 +0200

    some packages

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const appartementRoutes = require ('./src/routes/appartement.routes')
+const logger = require('./src/config/app.config').logger
+
+const app = express()
+const port = 8000
+
+app.use(express.json())
+
+// Generic endpoint handler - voor alle routes
+app.all('*', (req, res, next) => {
+    // logger.info('Generieke afhandeling aangeroepen!')
+    // ES16 deconstructuring
+    const { method, url } = req
+    logger.info(`${method} ${url}`)
+    next()
+})
+
+// Hier installeren we de routes
+app.use('/api/movies', appartementRoutes)
+
+// Handle endpoint not found.
+app.all('*', (req, res, next) => {
+    const { method, url } = req
+    const errorMessage = `${method} ${url} does not exist.`
+    logger.warn(errorMessage)
+    const errorObject = {
+        message: errorMessage,
+        code: 404,
+        date: new Date()
+    }
+    next(errorObject)
+})
+
+// Error handler
+app.use((error, req, res, next) => {
+    logger.error('Error handler: ', error.message.toString())
+    res.status(error.code).json(error)
+})
+
+app.listen(port, () => logger.info(`Example app listening on port ${port}!`))
+
+module.exports = app
